d2f7c8ecac00e198ede1484b1e1c7dbcc6b07e19
sreegetzu <110039225+sreegetzu@users.noreply.github.com>
2022-08-16 13:15:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "devops-aws-bucket"
     key = "main"
-    region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    region = "us-east-1"
+    dynamodb_table = "devops-db-table"
   }
 }
